6a76290da51c28c71eafe6f3a280a81e87dfd923
LVH-27 <reo.the.yokel@gmail.com>
2020-06-28 23:33:49 +0200

    omitted IP from DB init

--- app/nkosl-blog/app/__init__.py
+++ app/nkosl-blog/app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 nkosl_app = Flask(__name__)
 nkosl_app.config['SECRET_KEY'] = 'du_bist_mein_sofa'
-nkosl_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://nkosl:nkosl@10.0.3.25/nkosl"
+nkosl_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://nkosl:nkosl@<database_IP>/nkosl"
 nkosl_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(nkosl_app)
 migrate = Migrate(nkosl_app, db)
